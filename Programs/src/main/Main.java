16b2f40222e6e6148af72a49130cbb9699366c84
Ines Oliveira <ines.oliveira@grenoble-inp.org>
2024-01-24 17:04:31 +0100

    [MAIN] test parser (unfinished)

--- Programs/src/main/Main.java
+++ Programs/src/main/Main.java
@@ -1,7 +1,22 @@
 package src.main;
 
+import src.algorithms.AbstractAlgo;
+import src.problems.AbstractProblem;
+import src.utils.Parser;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
+        ArrayList<AbstractAlgo> algos = new ArrayList<>();
+        ArrayList<AbstractProblem> problems = new ArrayList<>();
+
+        problems.add(Parser.createProblem("Salle01", 1));
+        problems.add(Parser.createProblem("Salle01", 2));
+
+        Runner r1 = new Runner(algos, problems);
+
+        System.out.println(r1);
     }
 }

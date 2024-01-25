24e8f2d898d9f662f6a022c67bb5ee55a3824edd
Mélian Fruchart <melian.fruchart@grenoble-inp.org>
2024-01-25 17:30:27 +0100

    [MODEL] add new algo and test in Main
    Runner is now printing all its algorithms.

---
+++ Programs/src/algorithms/AlgoHeuristique1.java
@@ -0,0 +1,12 @@
+package src.algorithms;
+
+import src.problems.AbstractProblem;
+import src.problems.Solution;
+
+public class AlgoHeuristique1 extends AbstractAlgo {
+
+    @Override
+    public Solution execute(AbstractProblem problem) {
+        return null;
+    }
+}

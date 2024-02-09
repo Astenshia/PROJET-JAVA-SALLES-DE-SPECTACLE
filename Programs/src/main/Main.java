8f85a7fc5728e590a1806c5a14abbcc46b87a067
Ines Oliveira <ines.oliveira@grenoble-inp.org>
2024-02-09 17:08:12 +0100

    [Runner] Add printSynthesis to print runtimes

--- Programs/src/main/Main.java
+++ Programs/src/main/Main.java
@@ -1,7 +1,6 @@
 package src.main;
 
-import src.algorithms.AbstractAlgo;
-import src.algorithms.AlgoHeuristique1;
+import src.algorithms.*;
 import src.problems.AbstractProblem;
 import src.utils.Parser;
 
@@ -11,19 +10,23 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<AbstractAlgo> algos = new ArrayList<>();
         algos.add(new AlgoHeuristique1());
-        algos.add(new AlgoHeuristique1());
+        algos.add(new AlgoHeuristique2());
         // algos.add(new AlgoHeuristique2());
         // algos.add(new AlgoEnumTotale());
 
         ArrayList<AbstractProblem> problems = new ArrayList<>();
         problems.add(Parser.createProblem("Salle01", 1));
+        problems.add(Parser.createProblem("Salle02", 1));
+        problems.add(Parser.createProblem("Salle03", 1));
 
         Runner r1 = new Runner(algos, problems);
         System.out.println(r1);
 
         r1.executeAllAlgorithms();
         r1.printAllSolutions();
-        r1.storeAllSolutions();
+        //r1.storeAllSolutions();
+        r1.printSynthesis();
+
 
 
 
